2ec41d6284aeb3b2cf36872745dcc9badbf9e72d
ecosse3 <luk.kurpiewski@gmail.com>
2026-04-13 23:59:57 +0200

    chore(neovim): disable Ruby and Python3 support

--- home/neovim.nix
+++ home/neovim.nix
@@ -7,6 +7,8 @@
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    withRuby = false;
+    withPython3 = false;
     # No extraLuaConfig -- config lives in ~/.config/nvim (managed via stow)
     # No plugins -- managed in ~/.config/nvim directly
   };
